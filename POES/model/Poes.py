f59b4bbf8a54c158805bacd9af4bd8f36fdbd2df
Steven <stefceva@espol.edu.ec>
2024-10-27 14:33:30 -0500

    Poes Funtion was Create.

---
+++ POES/model/Poes.py
@@ -0,0 +1,21 @@
+#Funcion POES
+
+def poes(area, h, poro, swi, boi):
+    """
+
+    Parameters
+    ----------
+    area: Area in ft2
+    h: Thickness in ft
+    poro: Porosity (dimensionless)
+    swi: water saturation (dimensionless)
+    boi: oil bubble factor (bbl / stb)
+
+    Returns
+    -------
+    poes: Petroleum original in situ (bbl)
+
+    """
+    poes = (7758*area*h*poro*(1-swi))/boi
+
+    return area*h*poro*swi
